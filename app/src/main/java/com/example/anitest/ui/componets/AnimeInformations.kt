0c9eee0ce3fc4c68749109b8c597fa272e43d6c6
Xandro01 <alescicolone01@gmail.com>
2024-06-12 19:57:40 +0200

    Preview of title composable

--- app/src/main/java/com/example/anitest/ui/componets/AnimeInformations.kt
+++ app/src/main/java/com/example/anitest/ui/componets/AnimeInformations.kt
@@ -1,6 +1,7 @@
 package com.example.anitest.ui.componets
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -10,10 +11,15 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -26,7 +32,9 @@ import androidx.compose.ui.unit.sp
 @Preview
 @Composable
 fun AnimeTitles( /** name: String, titles: List<String> **/ ) {
-
+    var expandedTitles by remember {
+        mutableStateOf(false)
+    }
     val name =  "Shinjeki no Kyojin"
     val titles = listOf<String>("進撃の巨人", "Attack on Titan",  "OtherName")
     
@@ -55,10 +63,30 @@ fun AnimeTitles( /** name: String, titles: List<String> **/ ) {
             color = Color.White,
             fontSize = 20.sp
         )
-        Spacer(Modifier.weight(1f).background(Color.Green))
-
-        IconButton( onClick = { /*TODO*/ }) {
+        Spacer(
+            Modifier
+                .weight(1f)
+                .background(Color.Green))
+        Box(){
+            IconButton( onClick = { expandedTitles = expandedTitles.not() }) {
                 Icon(tint = Color.White, imageVector = Icons.Filled.MoreVert, contentDescription = "" )
+            }
+            DropdownMenu(
+                modifier = Modifier
+                    .padding(top = 2.dp)
+                    .clip(RoundedCornerShape(8.dp))
+                    .background(Color.Black)
+                    .padding(horizontal = 16.dp),
+                expanded = expandedTitles,
+                onDismissRequest = { expandedTitles = false }) {
+                titles.subList(1, titles.size).forEach {
+                    Text( modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 16.dp, bottom = 8.dp) , text = it, color = Color.White)
+
+                }
+            }
         }
+
     }
 }
